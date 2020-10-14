c7ff3dae6dae9a735eae5746d92c238ca4ab0242
dwsJason <JGAndersen@gmail.com>
2020-10-14 19:35:48 -0400

    import: empty template

---
+++ source/main.cpp
@@ -0,0 +1,15 @@
+//
+// km232 - Keyboard + Mouse Relay tool
+//
+// Grab Keyboard + Mouse events, and relay them out the serial port
+// to a Hagstrom USB-KM232, set especially to feed into a Wombat, for use with
+// the Apple IIgs
+//
+// Written by Jason Andersen  10/14/2020
+//
+
+int main(int argc, char* argv[])
+{
+	return 0;
+}
+
